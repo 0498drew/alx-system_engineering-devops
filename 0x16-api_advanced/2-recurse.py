057bbe57193996eecdacea401fa09a0811ce325d
0498drew <mwaseandrew98@gmail.com>
2023-02-01 15:23:35 -0800

    fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,24 +3,38 @@
     Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
+import requests
+
+
 def recurse(subreddit, hot_list=[], after=None):
+    """
+        Returns the number of hot posts for a given subreddit.
+        Returns 0 if invalid subreddit was given
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        'User-Agent': 'MyBot/0.0.1'
+            "User-Agent": "redditdev scraper by u/coderboy-exe",
+            "From": "coderboy.exe@gmail.com"
     }
     params = {
-        'limit': 100,
-        'after': after
+            "limit": 100
     }
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json', headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        after = data['data']['after']
-        for post in data['data']['children']:
-            hot_list.append(post['data']['title'])
-        if after:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
+
+    if after:
+        params["after"] = after
+
+    res = requests.get(url, headers=headers, params=params)
+    if res.status_code == 200:
+        data = res.json()
+
+        hot_posts = data.get("data", {}).get("children", [])
+        for post in hot_posts:
+            hot_list += post.get("data").get("title")
+
+        update_after = data.get("data").get("after")
+
+        if update_after:
+            return recurse(subreddit, hot_list, update_after)
     else:
         return None
-
+    return hot_list
